3b77563c826abcca98e35b69557f650a84f8d68d
Shahin Amon <amons@bzz.ch>
2022-10-07 15:29:34 +0200

    feat: login gemacht

--- src/login.js
+++ src/login.js
@@ -1,14 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
     checkLoggedIn();
     
-    document.getElementById("inputForm").addEventListener("submit", function(event){
+    document.getElementById("inputForm").addEventListener("submit", (event) =>{
         event.preventDefault();
 
         const email = document.getElementById("emailInput").value;
         const password = document.getElementById("passwordInput").value;
-        const login = {emial: email, password: password};
+        const login = {email: email, password: password};
         
-        //Login
         fetch("http://localhost:3000/auth/cookie/login", {
             method: "POST",
             credentials: "include",
@@ -17,9 +16,11 @@ document.addEventListener("DOMContentLoaded", () => {
             },
             body: JSON.stringify(login)
         })
+        .then(window.location.reload());
     })
 })
 
+//Schaut nach, ob man eingeloggt ist und passt je nach Status die Login Seite an
 function checkLoggedIn(){
     const loggedIn = document.getElementById("loggedIn");
     const notLoggedIn = document.getElementById("notLoggedIn");
@@ -30,11 +31,13 @@ function checkLoggedIn(){
     })
     .then((response) => {
         if(response.status === 401) {
+            //Nicht eingeloggt, also Login Seite anzeigen mit Input Feldern
             loggedIn.classList.add("hidden");
             notLoggedIn.classList.remove("hidden");
         } else {
+            //Eingeloggt, also entsprechende Nachricht zeigen
             loggedIn.classList.remove("hidden");
-            notLoggedIn.classList.add("hidden")
+            notLoggedIn.classList.add("hidden");
         }
     }
     )
